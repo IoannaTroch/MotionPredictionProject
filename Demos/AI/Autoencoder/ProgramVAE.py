b10f8ef8fec5436900c2413cb730ba6f0e5cc55b
BillVlachos <161938468+BillVlachos@users.noreply.github.com>
2026-05-31 15:53:38 +0300

    Update ProgramVAE.py

--- Demos/AI/Autoencoder/ProgramVAE.py
+++ Demos/AI/Autoencoder/ProgramVAE.py
@@ -231,10 +231,12 @@ class Program:
         #auta einai gia to plot
         plt.ioff() 
         plt.savefig("loss_history_VAE.png", dpi=300, bbox_inches='tight') 
+        #na apothikeusei to montelo afou exei ekpaideutei gia na to xrhsimopoihsw sto ervthma 4
+        torch.save(self.Network, "vae_full_model.pth")
+        print("The model was saved successfully!")
         plt.show() 
 
-        #na apothikeusei to montelo afou exei ekpaideutei gia na to xrhsimopoihsw sto ervthma 4
-        torch.save(self.Network.state_dict(), "vae_weights.pth") 
+
 
     def PlotTrainVal(self, train_losses, val_losses, epoch):
         #gia na kanei plot tis grafikes
